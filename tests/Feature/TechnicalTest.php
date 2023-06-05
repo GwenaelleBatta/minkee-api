dcdbba6a62637ad8ff9718a0db37623f3da0be73
Gwenbatta0704 <gwenaellebatta@gmail.com>
2023-06-05 13:13:00 +0200

    added technical test

---
+++ tests/Feature/TechnicalTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Models\Plan;
+use App\Models\Supply;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+uses(RefreshDatabase::class);
+
+it('is possible to display the list of words in the glossary', function () {
+    $this->artisan('migrate:fresh');
+    $this->seed([\Database\Seeders\GlossarySeeder::class]);
+    $response = $this->get('/api/glossaries');
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'data' => [
+            '*' => [
+                'id',
+                'name',
+                'slug',
+                'description',
+                'created_at',
+                'updated_at',
+            ],
+        ],
+    ]);
+});
+
+it('is possible to display the list of fabrics', function () {
+    $this->artisan('migrate:fresh');
+    $this->seed([\Database\Seeders\FabricSeeder::class]);
+    $response = $this->get('/api/fabrics');
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'data' => [
+            '*' => [
+                'id',
+                'name',
+                'slug',
+                'description',
+                'use',
+                'created_at',
+                'updated_at',
+            ],
+        ],
+    ]);
+});
+
+it('is possible to display the list of gradations', function () {
+    $this->artisan('migrate:fresh');
+    $this->seed([\Database\Seeders\GradationSeeder::class]);
+    $response = $this->get('/api/gradations');
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'data' => [
+            '*' => [
+                'id',
+                'name',
+                'slug',
+                'base',
+                'min',
+                'max',
+                'image',
+                'variation',
+                'created_at',
+                'updated_at',
+            ],
+        ],
+    ]);
+});
